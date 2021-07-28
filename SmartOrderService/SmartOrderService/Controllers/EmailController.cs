29949db12d944d56b015cb12604c1e0f744f5c5d
KevinPool <kevmkc@gmail.com>
2021-07-28 17:39:20 -0500

    Se agregó el Endpoint para enviar mensaje de bienvenida Dummy

---
+++ SmartOrderService/SmartOrderService/Controllers/EmailController.cs
@@ -0,0 +1,33 @@
+﻿using SmartOrderService.Models.Requests;
+using SmartOrderService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SmartOrderService.Controllers
+{
+    public class EmailController : ApiController
+    {
+        [HttpPost]
+        [Route("~/api/SendEmail/Wellcome")]
+        public IHttpActionResult SendWellcomeEmail(WellcomeEmailRequest request)
+        {
+            try
+            {
+                var service = new EmailService();
+                var response = service.SendWellcomeEmail(request);
+
+                if (response)
+                    return Ok();
+
+                return BadRequest();
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+    }
+}
---
+++ SmartOrderService/SmartOrderService/Services/EmailService.cs
@@ -0,0 +1,81 @@
+﻿using SmartOrderService.Models.Email;
+using SmartOrderService.Models.Requests;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace SmartOrderService.Services
+{
+    public class EmailService
+    {
+        public bool SendWellcomeEmail(WellcomeEmailRequest request)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("~/Content/Template/WellcomeEmail.html")))
+                {
+                    string body = reader.ReadToEnd();
+
+                    body = body.Replace("{CustomerName}", request.CustomerName);
+                    body = body.Replace("{TermsAndConsitionLink}", request.TermsAndConditionLink);
+                    body = body.Replace("{CanceledLink}", request.CanceledLink);
+
+                    var mailInfo = new SendAPIEmailrequest()
+                    {
+                        To = request.CustomerEmail,
+                        Subject = "¡Gracias por ser cliente Bepensa!",
+                        Body = body
+                    };
+
+                    DummySendEmail(mailInfo);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void DummySendEmail(SendAPIEmailrequest request)
+        {
+            MailMessage mmsg = new MailMessage();
+
+            mmsg.To.Add(request.To);
+            mmsg.Subject = request.Subject;
+            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            mmsg.Body = request.Body;
+            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+            mmsg.IsBodyHtml = true;
+
+            mmsg.From = new MailAddress("kevmkc2@gmail.com");
+
+            SmtpClient client = new SmtpClient();
+
+            client.Credentials = new NetworkCredential("kevmkc2@gmail.com", "kevinblablabla");
+
+            client.Port = 587;
+            client.EnableSsl = true;
+
+            client.Host = "smtp.gmail.com";
+
+            try
+            {
+                client.Send(mmsg);
+            }
+            catch (Exception e)
+            {
+                var error = e.Message;
+            }
+        }
+    }
+}
