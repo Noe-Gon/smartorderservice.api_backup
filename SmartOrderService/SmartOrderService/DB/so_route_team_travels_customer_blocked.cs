cf4e9625c809efaa737c80e8e1601f5632fedac2
KevinPool <kevmkc@gmail.com>
2021-08-27 16:34:53 -0500

    Bloqueo de un cliente NOTAS:   - Se agregó endpoint de bloqueo de cliente   - Se agregó endpoint de desbloqueo de cliente   - Se agregó endpoint de obtención de clientes bloqueados por otro usuario   - Se agregó enpoint de besbloqueo de clientes bloqueados en una jornado   - Al finalizar Jornada se desbloquea a los clientes relacionadas a esta

---
+++ SmartOrderService/SmartOrderService/DB/so_route_team_travels_customer_blocked.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SmartOrderService.DB
+{
+    public class so_route_team_travels_customer_blocked
+    {
+        [Column("workDayId")]
+        public Guid WorkDayId { get; set; }
+        public so_work_day WorkDay { get; set; }
+
+        [Column("inventoryId")]
+        public int InventoryId { get; set; }
+        public so_inventory Inventory { get; set; }
+
+        [Column("userId")]
+        public int UserId { get; set; }
+        public so_user User { get; set; }
+
+        [Column("customerId")]
+        public int CustomerId { get; set; }
+        public so_customer Customer { get; set; }
+
+        [Column("created_date")]
+        public DateTime CreatedDate { get; set; }
+    }
+}
