ac6502a08c6195b2578f408842301623d0b0282a
irvingmagana <imagana@walook.com.mx>
2021-09-09 13:44:04 -0500

    Se suben avances de desarrollo del EndPoint para devolver las ventas de todos los clientes

---
+++ SmartOrderService/SmartOrderService/Models/DTO/SaleDto.cs
@@ -0,0 +1,20 @@
+﻿using using System.Collections.Generic;
+
+namespace SmartOrderService.Models.DTO
+{
+    public class SaleDto
+    {
+        public int UserId { get; set; }
+        public double TotalCash { get; set; }
+        public int SaleId { get; set; }
+        public double TotalCredit { get; set; }
+        public string CustomerTag { get; set; }
+        public int InventoryId { get; set; }
+        public string Date { get; set; }
+        public int CustomerId { get; set; }
+        public int DeliveryId { get; set; }
+        public List<SaleDetail> saleDetails { get; set; }
+        public List<SaleReplacement> saleReplacements { get; set; }
+        public List<SalePromotion> salePromotion { get; set; }
+    }
+}
