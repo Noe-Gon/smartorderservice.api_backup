3efa9e1180dc4bb05b08eadb8b7d8c22f92c245d
Noe-Gon <ngonzalez@walook.com.mx>
2022-03-15 12:05:45 -0600

    Se agregan el nuevo contexto de la tabla para la recuperación de los tokens, se agregó el controlador y el servicio correspondiente. se agregó el response del servicio

---
+++ SmartOrderService/SmartOrderService/DB/Configuracion_WorkByCloud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartOrderService.DB
+{
+    public class Configuracion_WorkByCloud
+    {
+        [Key]
+        public int wbcConfId { get; set; }
+        [StringLength(100)]
+        public string BillPocket_TokenDispositivo { get; set; }
+        [StringLength(100)]
+        public string BillPocket_TokenUsuario { get; set; }
+        public DateTime? createdon { get; set; }
+        public int userId { get; set; }
+        public virtual so_user so_user { get; set; }
+    }
+}
---
+++ SmartOrderService/SmartOrderService/Services/BillPocketService.cs
@@ -0,0 +1,36 @@
+﻿using SmartOrderService.CustomExceptions;
+using SmartOrderService.DB;
+using SmartOrderService.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartOrderService.Services
+{
+    public class BillPocketService
+    {
+        SmartOrderModel db = new SmartOrderModel();
+
+        public ResponseBase<BillPocketTokensResponse> GetTokensByUserId(int userId)
+        {
+            var user = db.Configuracion_WorkByCloud.Where(x => x.userId == userId).FirstOrDefault();
+
+            if (user == null)
+            {
+                return ResponseBase<BillPocketTokensResponse>.Create(new BillPocketTokensResponse
+                {
+                    BillPocket_TokenDispositivo = user.BillPocket_TokenDispositivo,
+                    BillPocket_TokenUsuario = user.BillPocket_TokenUsuario
+                });
+            }
+            else
+            {
+                return ResponseBase<BillPocketTokensResponse>.Create(new List<string>
+                {
+                    "No se logró encontrar al usuario."
+                });
+            }
+        }
+    }
+}
