b7c59d3f373e8a6db074801c3b4d106bd21b6806
KevinPool <kevmkc@gmail.com>
2022-05-25 12:41:02 -0500

    Se agregó algunos modelos

---
+++ SmartOrderService/SmartOrderService/DB/so_synchronized_consumer_detail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SmartOrderService.DB
+{
+    public class so_synchronized_consumer_detail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int synchronizedDetailId { get; set; }
+        public int synchronizedId { get; set; }
+        public int userId { get; set; }
+        public bool synchronized { get; set; }
+    }
+}
