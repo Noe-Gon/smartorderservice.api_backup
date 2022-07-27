bac16b39e6384b69f41066a76187cc58f5c9ccb6
KevinPool <kevmkc@gmail.com>
2022-07-27 11:57:19 -0500

    Se cambio el nombre de la columna

--- SmartOrderService/SmartOrderService/DB/so_portal_links_log.cs
+++ SmartOrderService/SmartOrderService/DB/so_portal_links_log.cs
@@ -10,7 +10,7 @@ namespace SmartOrderService.DB
     {
         public Guid Id { get; set; }
 
-        [Column("created_date")]
+        [Column("createdon")]
         public DateTime CreatedDate { get; set; }
 
         [Column("limit_days")]
