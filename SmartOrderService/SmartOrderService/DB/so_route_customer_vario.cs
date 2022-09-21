3aeb7e420597446e13b077d9a5ff094168afde6d
KevinPool <kevmkc@gmail.com>
2022-09-21 17:39:30 -0500

    Se agregó la logica de price list NOTAS:  - Se agregó en el registro del cliente

---
+++ SmartOrderService/SmartOrderService/DB/so_route_customer_vario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartOrderService.DB
+{
+    public class so_route_customer_vario
+    {
+        [Column("routecustomervarioId")]
+        public int Id { get; set; }
+
+        [Column("createdon")]
+        public DateTime? CreatedOn { get; set; }
+
+        [Column("createdby")]
+        public int? CreatedBy { get; set; }
+
+        [Column("modifiedon")]
+        public DateTime? ModifiedOn { get; set; }
+
+        [Column("modifiedby")]
+        public int? ModifiedBy { get; set; }
+
+        [Column("status")]
+        public bool Status { get; set; }
+
+        #region Relations
+
+        [Column("customerId")]
+        public int CustomerId { get; set; }
+
+        public so_customer Customer { get; set; }
+
+        [Column("routeId")]
+        public int RouteId { get; set; }
+
+        public so_route Route { get; set; }
+        #endregion
+    }
+}
