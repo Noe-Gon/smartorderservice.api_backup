1fbf347561499df844f020005f184578a957d39d
KevinPool <kevmkc@gmail.com>
2023-07-24 01:31:16 -0600

    Correccion de crews

--- SmartOrderService/SmartOrderService/Services/Ope20Service.cs
+++ SmartOrderService/SmartOrderService/Services/Ope20Service.cs
@@ -18,40 +18,22 @@ namespace SmartOrderService.Services
     {
         public bool CallCrewOpe20(CrewOpe20Request request)
         {
-            //var client = new RestClient();
-            //client.BaseUrl = new Uri(ConfigurationManager.AppSettings["API_OPE20_URL"]);
-            //string SubscriptionKey = ConfigurationManager.AppSettings["API_OPE20_subscription_key"];
-            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-            //var requestConfig = new RestRequest("api/crew", Method.PUT);
-            //requestConfig.RequestFormat = DataFormat.Json;
-            //requestConfig.AddHeader("Ocp-Apim-Subscription-Key", SubscriptionKey);
-            //requestConfig.AddJsonBody(request);
+            var client = new RestClient();
+            client.BaseUrl = new Uri(ConfigurationManager.AppSettings["API_OPE20_URL"]);
+            string SubscriptionKey = ConfigurationManager.AppSettings["API_OPE20_subscription_key"];
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            var requestConfig = new RestRequest("api/crew", Method.PUT);
+            requestConfig.RequestFormat = DataFormat.Json;
+            requestConfig.AddHeader("Ocp-Apim-Subscription-Key", SubscriptionKey);
+            requestConfig.AddJsonBody(request);
 
-            //var RestResponse = client.Execute(requestConfig);
-            //if (RestResponse.StatusCode == System.Net.HttpStatusCode.OK)
-            //{
-            //    return true;
-            //}
-            
-                var httpClient = new HttpClient();
-                httpClient.BaseAddress = new Uri(ConfigurationManager.AppSettings["API_OPE20_URL"]);
-                string SubscriptionKey = ConfigurationManager.AppSettings["API_OPE20_subscription_key"];
+            var RestResponse = client.Execute(requestConfig);
+            if (RestResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
 
-                httpClient.DefaultRequestHeaders.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
-
-                var jsonContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
-
-                HttpResponseMessage response = httpClient.PutAsync("api/crew", jsonContent).Result;
-
-                if (response.IsSuccessStatusCode)
-                {
-                    return true;
-                }
-
-                throw new ExternalAPIException(response.StatusCode.ToString() + ": " + response.ReasonPhrase);
-            
+            throw new ExternalAPIException(RestResponse.StatusCode.ToString() + ": " + RestResponse.Content);
         }
 
         public bool IsCediInOpe20(string code)
