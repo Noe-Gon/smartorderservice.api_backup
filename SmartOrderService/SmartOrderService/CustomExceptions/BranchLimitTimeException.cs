f6fc1cf218abd3e8d2d904baf82fa0cf120089ef
KevinPool <kevmkc@gmail.com>
2023-07-24 01:08:55 -0600

    Se Solucionó el Limit Time y agregó el codigo de error correspondiente

---
+++ SmartOrderService/SmartOrderService/CustomExceptions/BranchLimitTimeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartOrderService.CustomExceptions
+{
+    public class BranchLimitTimeException : Exception
+    {
+        public BranchLimitTimeException() : base()
+        {
+
+        }
+
+        public BranchLimitTimeException(string message) : base(message)
+        {
+
+        }
+    }
+}
