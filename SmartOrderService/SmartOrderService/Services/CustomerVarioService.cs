1c5d4c16db7b75916166d8697f883ef449e708b5
Noe-Gon <ngonzalez@walook.com.mx>
2024-02-19 17:38:32 -0600

    subiendo cambios hotfix cliente vario Noé

---
+++ SmartOrderService/SmartOrderService/Services/CustomerVarioService.cs
@@ -0,0 +1,44 @@
+﻿using SmartOrderService.CustomExceptions;
+using SmartOrderService.DB;
+using SmartOrderService.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartOrderService.Services
+{
+    public sealed class CustomerVarioService
+    {
+        private SmartOrderModel db = new SmartOrderModel();
+        public int GetCustomerVarioIdByRouteId(int routeId)
+        {
+            var customerVario = db.so_customer.Where(x => x.name == "cliente_vario" && x.status).ToList();
+            var customerVarioIds = customerVario.Select(x => x.customerId).ToList();
+            var routeCustomerLst = db.so_route_customer.Where(x => customerVarioIds.Contains(x.customerId)).ToList();
+            var routeCustomer = routeCustomerLst.Where(x => x.routeId == routeId).FirstOrDefault();
+            if (routeCustomer == null)
+            {
+                throw new NotFoundVarioRouteException($"La ruta con identificador {routeId} no tienen configurado un cliente vario");
+            }
+            return routeCustomer.customerId;
+        }
+
+        public so_route_customer GetCustomerVarioForCreate(UoWConsumer UoWConsumer, int routeId)
+        {
+            var customerVario = UoWConsumer.RouteCustomerRepository
+                    .Get(x => x.routeId == routeId && x.so_customer.name == "cliente_vario")
+                    .FirstOrDefault();
+            return customerVario;
+        }
+
+        public so_route_customer GetCustomerVarioForCreate(SmartOrderModel db, int routeId)
+        {
+            var customerVario = db.so_route_customer
+                    .Where(x => x.routeId == routeId && x.so_customer.name == "cliente_vario")
+                    .FirstOrDefault();
+            return customerVario;
+        }
+
+    }
+}
